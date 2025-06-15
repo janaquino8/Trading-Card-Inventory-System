4a04b26e093ca0a3d20f2765f5c00c84ba202237
JanAquinoDLSU <jan_leoric_b_aquino@dlsu.edu.ph>
2025-06-15 17:41:06 +0800

    refactored main and src folder

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 package src;
 
+import src.controller.Controller;
+
 // Main class
 public class Main {
     public static void main(String[] args) {
-
+        Controller controller = new Controller();
+        controller.run();
     }
 }
