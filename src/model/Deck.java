c6ebcac6339a99d8d2a8a8a690459215fb3d7b94
JanAquinoDLSU <jan_leoric_b_aquino@dlsu.edu.ph>
2025-06-21 22:42:43 +0800

    2st batch of modifications (deck)

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -10,4 +10,53 @@ public class Deck {
     public Deck() {
         cards = new Card[MAX_COUNT];
     }
+
+    public Deck(String name) {
+        this();
+        this.setName(name);
+    }
+
+    public void addCard(Card card) {
+        int index = 0;
+
+        while (this.getCard(index) != null) {
+            index++;
+        }
+
+        this.setCard(index, card);
+    }
+
+    public void removeCard(int index) {
+        int i = index + 1;
+        while (i < MAX_COUNT && this.getCard(i) != null) {
+            this.setCard(i - 1, this.getCard(i));
+            i++;
+        }
+        this.setCard(i - 1, null);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Card getCard(int index) {
+        if (index >= 0 && index < MAX_COUNT) {
+            return this.cards[index];
+        }
+        return null;
+    }
+
+    public void setCard(int index, Card card) {
+        if (index >= 0 && index < MAX_COUNT) {
+            this.cards[index] = card;
+        }
+    }
+
+    public Card[] getCards() {
+        return this.cards;
+    }
 }
