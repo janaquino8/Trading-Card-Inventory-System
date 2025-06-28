6c0278e03b48f7082d7037ad9fad33c61ff67697
JanAquinoDLSU <jan_leoric_b_aquino@dlsu.edu.ph>
2025-06-28 20:18:31 +0800

    29th - mco1 final stretch 2

--- src/model/Rarity.java
+++ src/model/Rarity.java
@@ -1,45 +1,55 @@
 package src.model;
 
+/*
+Rarity - Represents card rarity levels with its names and abbreviations
+ */
 public class Rarity {
     private String name;
-    private String abbreviation;
 
+    /*
+    Method  - Rarity (constructor)
+    Method Comment : Default constructor creates a Common rarity
+     */
     public Rarity() {
         this.setName("Common");
-        this.setAbbreviation("COM");
     }
 
+    /*
+    Method      - Rarity (constructor)
+    int rarity  - Numeric code representing rarity level (2-4)
+    Method Comment : Creates specific rarity based on input code (2: Uncommon, 3: Rare, 4: Legendary)
+     */
     public Rarity(int rarity) {
         this();
         switch (rarity) {
             case 2:
                 this.setName("Uncommon");
-                this.setAbbreviation("UCM");
                 break;
             case 3:
                 this.setName("Rare");
-                this.setAbbreviation("RAR");
                 break;
             case 4:
                 this.setName("Legendary");
-                this.setAbbreviation("LEG");
                 break;
         }
     }
 
+    /*
+    Method       - getName
+    return type  - String
+    Method Comment : Returns the full name of the rarity level
+     */
     public String getName() {
         return this.name;
     }
 
+    /*
+    Method       - setName
+    String name  - New name to assign to the rarity
+    return type  - void
+    Method Comment : Sets the rarity's name
+     */
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getAbbreviation() {
-        return this.abbreviation;
-    }
-
-    public void setAbbreviation(String abbreviation) {
-        this.abbreviation = abbreviation;
     }
 }
--- src/model/Variant.java
+++ src/model/Variant.java
@@ -1,56 +1,77 @@
 package src.model;
 
+/*
+Variant - Represents different card variants with its name, abbreviations, and unique value multipliers
+ */
 public class Variant {
     private String name;
-    private String abbreviation;
     private double valueMultiplier;
 
+    /*
+    Method  - Variant (constructor)
+    Method Comment : Default constructor creates a Normal variant with 1x multiplier
+     */
     public Variant() {
         this.setName("Normal");
-        this.setAbbreviation("N");
         this.setValueMultiplier(1);
     }
 
+    /*
+    Method       - Variant (constructor)
+    int variant  - Numeric code representing variant type (2-4)
+    Method Comment : Creates specific variant based on input code (2: Extended-art, 3: Full-art, 4: Alt-art)
+     */
     public Variant(int variant) {
         switch (variant) {
             case 2:
                 this.setName("Extended-art");
-                this.setAbbreviation("EA");
                 this.setValueMultiplier(1.5);
                 break;
             case 3:
                 this.setName("Full-art");
-                this.setAbbreviation("FA");
                 this.setValueMultiplier(2);
                 break;
             case 4:
                 this.setName("Alt-art");
-                this.setAbbreviation("AA");
                 this.setValueMultiplier(3);
                 break;
         }
     }
 
+    /*
+    Method       - getName
+    return type  - String
+    Method Comment : Returns the full name of the variant
+     */
     public String getName() {
         return this.name;
     }
 
+    /*
+    Method       - setName
+    String name  - New name to assign to the variant
+    return type  - void
+    Method Comment : Sets the variant's name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getAbbreviation() {
-        return this.abbreviation;
-    }
-
-    public void setAbbreviation(String abbreviation) {
-        this.abbreviation = abbreviation;
-    }
-
+    /*
+    Method       - getValueMultiplier
+    return type  - double
+    Method Comment : Returns the multiplier applied to card's base value
+     */
     public double getValueMultiplier() {
         return this.valueMultiplier;
     }
 
+    /*
+    Method                  - setValueMultiplier
+    double valueMultiplier  - New multiplier value to assign
+    return type             - void
+    Method Comment : Sets the value multiplier for this variant
+     */
     public void setValueMultiplier(double valueMultiplier) {
         this.valueMultiplier = valueMultiplier;
     }
