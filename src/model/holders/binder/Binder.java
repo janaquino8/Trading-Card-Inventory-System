f20086ec714ba882a50afe003da1e183a1617170
John Lorens Tee <john_lorens_tee@dlsu.edu.ph>
2025-07-29 17:03:46 +0800

    ??th - Removed trade from Binder.java

--- src/model/holders/binder/Binder.java
+++ src/model/holders/binder/Binder.java
@@ -29,16 +29,4 @@ public abstract class Binder extends Holder {
         Arrays.sort(this.getCards(), new NameSorter());
     }
 
-    /**
-     * trade
-     * @param outgoingCardIndex the index of the card to be removed from the binder
-     * @param incomingCard card to be added to the binder
-     * Executes a trade between a card in the binder and a card outside the collection
-     */
-    public void trade(int outgoingCardIndex, Card incomingCard) {
-        if (this.getCard(outgoingCardIndex) != incomingCard) {
-            this.setCard(outgoingCardIndex, incomingCard);
-        }
-    }
-
 }
