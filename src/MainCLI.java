7a450e484d54da6074167ac793b762a2b7b3ea6d
Jan Aquino <jan_leoric_b_aquino@dlsu.edu.ph>
2025-09-07 22:40:43 +0800

    feat: CLI version

---
+++ src/MainCLI.java
@@ -0,0 +1,27 @@
+/*
+    TRADING CARD INVENTORY SYSTEM
+    Jan Leoric B. Aquino | John Lorens S. Tee
+
+    This is a trading card inventory system that allows the user to perform actions similar to real-life trading card collecting.
+*/
+
+package src;
+
+import src.controller.OldController;
+
+/**
+ * Contains the main method which initiates running of program
+ *
+ * <p> Due to partial completion of the GUI, the CLI version of the program is used at the moment. You may test the
+ * existing GUI functionality by commenting out lines 25-26 and uncommenting lines 29-30
+ */
+public class MainCLI {
+    /**
+     * Main method of the program
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+        // CLI Version
+        OldController controller = new OldController();
+    }
+}
